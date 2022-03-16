a5087a5c6880f6228c8d8c81b0c5d6780324c668
StewartBarker <50987431+StewartBarker@users.noreply.github.com>
2022-03-16 08:39:58 +0000

    remove commented out code

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
 const express = require('express');
 const router = require('./routes/locationRoutes');
-// const bpdtsService = require('./services/bpdtsService');
 
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 app.use('/', router);
-
-// app.get('/', (req, res) => {
-//   res.send(bpdtsService);
-// });
 
 app.listen(port, () => {
   console.log(`Runnning on port: ${port}`);

b1f48db5456c48ec4b25ddb9557190740b9b95be
StewartBarker <50987431+StewartBarker@users.noreply.github.com>
2022-03-20 11:37:39 +0000

    Add swagger docs

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const router = require('./routes/locationRoutes');
 
+const swaggerUi = require('swagger-ui-express'),
+swaggerDocument = require('./swagger.json');
+
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 app.use('/api/v1', router);
+app.use(
+  '/api-docs',
+  swaggerUi.serve, 
+  swaggerUi.setup(swaggerDocument)
+);
+
 
 const server = app.listen(port, () => {
   console.log(`Runnning on port: ${port}`);

fcf43e032c7a6ae688048c6bed0fcad3b8e6caa6
StewartBarker <50987431+StewartBarker@users.noreply.github.com>
2022-03-20 12:23:17 +0000

    Update port for npm test

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ swaggerDocument = require('./swagger.json');
 
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 app.use('/api/v1', router);
 app.use(

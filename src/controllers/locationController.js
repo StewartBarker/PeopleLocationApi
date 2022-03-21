8ae12ca16fd1b65ac518ced5abca85d9734776d4
StewartBarker <50987431+StewartBarker@users.noreply.github.com>
2022-03-21 09:24:31 +0000

    Add error handling and remove IP and email

--- src/controllers/locationController.js
+++ src/controllers/locationController.js
@@ -3,18 +3,25 @@ const geoLocationService = require('../services/geoLocationService');
 
 module.exports = {
   get: async (req, res) => {
-    const users = await bpdtsService.getUsers();
+    try {
+      const users = await bpdtsService.getUsers();
 
-    const london = { latitude: 51.509865, longitude: -0.118092 };
+      const london = { latitude: 51.509865, longitude: -0.118092 };
 
-    const usersWithin50MiesOfLondon = geoLocationService
-      .getUsersWithinXMileOfLocation(users, 50, london);
+      const usersWithin50MiesOfLondon = geoLocationService
+        .getUsersWithinXMileOfLocation(users, 50, london);
 
-    const usersInLondon = await bpdtsService.getUsersInCity('London');
-    
-    const allDistinctUsersInLondonOrWithin50MilesOfLondon = [...new Set([...usersWithin50MiesOfLondon, ...usersInLondon])];
+      const usersInLondon = await bpdtsService.getUsersInCity('London');
 
-    res.status(200);
-    return res.send(allDistinctUsersInLondonOrWithin50MilesOfLondon);
+      const allDistinctUsersInLondonOrWithin50MilesOfLondon = [...new Set([...usersWithin50MiesOfLondon, ...usersInLondon])];
+
+      const removedIpAndEmailForSecurityReasons = allDistinctUsersInLondonOrWithin50MilesOfLondon.map(({ email, ip_address, ...rest }) => rest);
+
+      res.status(200);
+      return res.send(removedIpAndEmailForSecurityReasons);
+    }
+    catch (error) {
+      return res.status(500).send(error);
+    }
   },
 };
